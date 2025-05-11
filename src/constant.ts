4abcc3a46f2ec4476593733f05fba87b7fcfcec6
harshdev-7275 <harsh.dev7275@gmail.com>
2025-05-11 13:28:20 +0530

    added constant

--- src/constant.ts
+++ src/constant.ts
@@ -1,1 +1,1 @@
-export const API_URL = import.meta.env.VITE_BASE_URL || 'https://insight-backend-1.onrender.com/api/v1';
+export const API_URL = import.meta.env.VITE_BASE_URL || 'https://insight-backend-1.onrender.com//v1/api';
